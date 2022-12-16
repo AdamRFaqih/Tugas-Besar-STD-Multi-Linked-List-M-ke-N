3d40715299bc1815c5504380294276b8124beae1
AdamRFaqih <90492899+AdamRFaqih@users.noreply.github.com>
2022-12-16 16:33:26 +0700

    add another files
    add pokemon.cpp and trainer.cpp also little implement

---
+++ pokemon.cpp
@@ -0,0 +1,12 @@
+#include "pokemon.h"
+
+void createListPokemon(ListPokemon &L){
+    L.first = NULL;
+}
+
+adrPokemon createNodePokemon(string p[3][151]){
+    adrPokemon newPokemon = new nodePokemon;
+}
+
+void insertFirstPokemon(ListPokemon &L, adrPokemon newPokemon);
+void showPokemon(ListPokemon &L);
